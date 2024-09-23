6fe5c6c8077f885d601e7da59c60bd2395d2f7c3
Adelina Gutu <w1705972@gmail.com>
2024-09-23 17:06:56 +0100

    coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = generate_answer(@question)
+    render :answer
+  end
+
+  def generate_answer(question)
+    if question == "I am going to work"
+      "Great!"
+    elsif question.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "ask", to: "questions#ask"
+  post "answer", to: "questions#answer"
 end
